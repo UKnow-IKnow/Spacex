7ff03d5120512ce20443868a222b52373adeff6d
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-03 15:57:08 +0530

    animation not working

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function(){
+
+    let socials = document.querySelector('.social div')
+
+    socials.forEach(function(social){
+        social.style.animation = 'moveIn 1s ease-in-out forwards'
+    })
+
+
+
+}())
