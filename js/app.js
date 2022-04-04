507acb5b155369d570d8b241bd0a273f2639ef61
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-04 22:58:03 +0530

    design fromt screen

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@
 
     let socials = document.querySelector('.social div')
 
-    socials.forEach(function(social){
-        social.style.animation = 'moveIn 1s ease-in-out forwards'
+    socials.forEach(function(social, index){
+        social.style.animation = 'moveIn 1s ease-in-out forwards ${index}s'
     })
-
-
 
 }())
