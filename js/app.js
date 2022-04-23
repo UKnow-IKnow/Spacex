3626ace97213bce4e17492e19478c644b2255028
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-23 12:08:16 +0530

    java scrpt not working

--- js/app.js
+++ js/app.js
@@ -6,4 +6,19 @@
         social.style.animation = 'moveIn 1s ease-in-out forwards ${index}s'
     })
 
+    let rocketPieces = document.querySelectorAll('.rocket-body span')
+
+    let rocket =document.querySelector('.rocket')
+
+    let animationStrt = window.innerHeight / 5 ;
+
+    let rocketoffSetTop = rocket.offsetTop ;
+
+    document.addEventListener('scroll', (e) => {
+        if(window.scroll > (rocketoffSetTop - animationStrt)){
+            rocketPieces[0].classList.add('active')
+            rocketPieces[1].classList.add('active')
+        }
+    })
+
 }())
